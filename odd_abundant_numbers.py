ca21379adc0b84376eab10777de1b05c77364dc2
dronrp <64811222+dronrp@users.noreply.github.com>
2021-01-07 13:12:15 +0000

    Add files via upload

---
+++ odd_abundant_numbers.py
@@ -0,0 +1,25 @@
+import math
+
+def numberDivisors(n):
+    divisors = [1]
+    i = 3
+    while i <= math.sqrt(n):
+        if n%i == 0:
+            if n/i == i:
+                divisors.append(i)
+            else:
+                divisors.extend([i,n//i])
+
+        i+=2
+    return sum(divisors)
+
+def oddAbundantNumbers():
+    ans = []
+    for i in range(9, 100000, 2):
+        if i < numberDivisors(i):
+            ans.append(i)
+
+    print(ans)
+
+if __name__ == "__main__":
+    oddAbundantNumbers()
